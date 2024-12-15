d4045db188de57871ff5fefb559ff25847520931
azrul <azrul@SYX>
2024-12-15 17:23:51 +0800

    Add project files.

---
+++ VehicleSalesAPI/Controllers/SalesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VehicleSalesAPI.Models;
+
+namespace VehicleSalesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class SalesController : ControllerBase
+    {
+        [HttpGet("SGetSales")]
+        public IActionResult GetSales()
+        {
+            var salesData = VehicleSales.GetSampleData();
+            return Ok(salesData);
+        }
+    }
+}
---
+++ VehicleSalesAPI/Models/VehicleSales.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace VehicleSalesAPI.Models
+{
+    public class VehicleSales
+    {
+        public int Year { get; set; }
+        public int Quarter { get; set; }
+        public string Region { get; set; }
+        public string Model { get; set; }
+        public int UnitsSold { get; set; }
+
+        public static List<VehicleSales> GetSampleData()
+        {
+            return new List<VehicleSales>
+            {
+                new VehicleSales { Year = 2021, Quarter = 1, Region = "North", Model = "Sedan", UnitsSold = 120 },
+                new VehicleSales { Year = 2021, Quarter = 2, Region = "South", Model = "SUV", UnitsSold = 95 },
+                // Add more sample data as needed
+            };
+        }
+    }
+}
